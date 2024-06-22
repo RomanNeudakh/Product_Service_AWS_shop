b7e46c5db79d6f5dd2df842690cb25126ae3d8e9
RomanNeudakh <neudakhroman@gmail.com>
2024-06-22 16:33:07 +0200

    feat: script to fill tables with test data was added

--- README.md
+++ README.md
@@ -16,10 +16,16 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 Enjoy!
 
+install dependencies from requirements.txt:
+```pip install -r requirements.txt```
+to deploy this stack to your default AWS account/region:
+```cdk deploy --all```
+to fill the created tables (Product_Cross_Check_table, Stock_Cross_Check_table) with test data, run the script in the root directory:
+```python fill_tables.py```
+
+
 In order to run unit tests, install libraries from requirements-dev.txt
-
 ```pip install -r requirements-dev.txt```
-
 Then run pytest in the root directory:
-
 ```pytest .```
+
--- product_service_aws_shop/lambda_functions/deployment_stack.py
+++ product_service_aws_shop/lambda_functions/deployment_stack.py
@@ -1,15 +1,26 @@
 from aws_cdk import (
     Stack,
 )
+import boto3
 from constructs import Construct
 from .get_product_by_id import GetProductById
 from .get_products_list import GetProductsList
 from .apigateway_stack import ApiGatewayStack
+from .create_table import CreateTable
 
 class DeploymentStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        session = boto3.Session()
+        region = session.region_name
+        dynamodb = boto3.client('dynamodb', region_name=region)
+        existing_tables = dynamodb.list_tables()['TableNames']
+        if 'Product_Cross_Check_table' not in existing_tables:
+            CreateTable(self, 'CreateProductTable', 'Product_Cross_Check_table', 'product_id')
+        if 'Stock_Cross_Check_table' not in existing_tables:
+            CreateTable(self, 'CreateStockTable', 'Stock_Cross_Check_table', 'id')
+        
         get_products_by_id_stack = GetProductById(self, 'GetProductByIdStackCrosscheck')
         get_products_list_stack = GetProductsList(self, 'GetProductsListStackCrosscheck')
         ApiGatewayStack(
