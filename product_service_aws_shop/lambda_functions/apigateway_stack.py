af6fba7a24e1340db87cf20bcf786840f42986eb
RomanNeudakh <neudakhroman@gmail.com>
2024-06-23 16:28:09 +0200

    feat: post validation and transaction based creation were added

--- product_service_aws_shop/lambda_functions/apigateway_stack.py
+++ product_service_aws_shop/lambda_functions/apigateway_stack.py
@@ -19,10 +19,40 @@ class ApiGatewayStack(Stack):
                 allow_headers=["*"]
             )
         )
+        request_model = api.add_model(
+            "RequestModel",
+            content_type="application/json",
+            model_name="RequestModel",
+            schema=apigateway.JsonSchema(
+                schema=apigateway.JsonSchemaVersion.DRAFT4,
+                title="Product",
+                type=apigateway.JsonSchemaType.OBJECT,
+                properties={
+                    "count": apigateway.JsonSchema(type=apigateway.JsonSchemaType.NUMBER, minimum=0),
+                    "price": apigateway.JsonSchema(type=apigateway.JsonSchemaType.NUMBER, minimum=1),
+                    "description": apigateway.JsonSchema(type=apigateway.JsonSchemaType.STRING),
+                    "title": apigateway.JsonSchema(type=apigateway.JsonSchemaType.STRING)
+                },
+                required=["count", "price", "description", "title"],
+                additionalProperties=False
+            )
+        )
+        request_validator = apigateway.RequestValidator(
+            self, "RequestValidator",
+            rest_api=api,
+            request_validator_name="RequestValidator",
+            validate_request_body=True,
+            validate_request_parameters=False
+        )
 
         create_product_integration = apigateway.LambdaIntegration(create_product_lambda)
         create_product_resource = api.root.add_resource("products")
-        create_product_resource.add_method("POST", create_product_integration)
+        create_product_resource.add_method(
+            "POST", 
+            create_product_integration, 
+            request_validator=request_validator,
+            request_models={"application/json": request_model}
+        )
         get_products_list_integration = apigateway.LambdaIntegration(get_products_list_lambda)
         create_product_resource.add_method("GET", get_products_list_integration)
 
--- product_service_aws_shop/lambda_handlers/create_product_handler.py
+++ product_service_aws_shop/lambda_handlers/create_product_handler.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 import json
 import logging
 import os
@@ -11,36 +10,50 @@ def createProductHandler(event, context):
     logger.info(f"Received event: {json.dumps(event)}")
     logger.info(f"Context: {context}")
     try:
-        dynamodb = boto3.resource('dynamodb', region_name=os.getenv('Region_name'))
+        dynamodb = boto3.client('dynamodb', region_name=os.getenv('Region_name'))
         stock_table_name = os.getenv('Stock_table_name')
         product_table_name = os.getenv('Product_table_name')
         if not stock_table_name or not product_table_name:
             raise ValueError("Environment variables for table names are not set.")
-        stock_table = dynamodb.Table(stock_table_name)
-        product_table = dynamodb.Table(product_table_name)
         body = json.loads(event['body'])
         product_id = str(uuid.uuid4())
         title = body['title']
         description = body.get('description', '')
         price = body['price']
-        product = {
-            'id': product_id,
-            'title': title,
-            'description': description,
-            'price': Decimal(str(price))
-        }
-        product_table.put_item(Item=product)
-        logger.info(f"Product created: {product}")
         count = body['count']
-        stock = {
-            'product_id': product_id,
-            'count': Decimal(str(count))
+        product_item = {
+            'id': {'S': product_id},
+            'title': {'S': title},
+            'description': {'S': description},
+            'price': {'N': str(price)}
         }
-        stock_table.put_item(Item=stock) 
-        logger.info(f"Product was added: {product}")
+        
+        stock_item = {
+            'product_id': {'S': product_id},
+            'count': {'N': str(count)}
+        }
+        logger.info(f"Product was added: {product_item}")
+        response = dynamodb.transact_write_items(
+            TransactItems=[
+                {
+                    'Put': {
+                        'TableName': product_table_name,
+                        'Item': product_item
+                    }
+                },
+                {
+                    'Put': {
+                        'TableName': stock_table_name,
+                        'Item': stock_item
+                    }
+                }
+            ]
+        )
+        logger.info(f"Transaction response: {response}")
+        logger.info(f"Product created: {product_item}")
         return {
             "statusCode": 200,
-            "body": 'New product was added',
+            "body": f'New product was added: {product_item}',
             "headers": {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Methods": "POST",
